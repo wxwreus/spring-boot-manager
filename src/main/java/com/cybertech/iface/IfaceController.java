fc1ef90afb37e4039a139877cc8eac83dcd7e96a
wxwreus@163.com <wxwreus@163.com>
2018-12-13 14:32:10 +0800

    对外接口

---
+++ src/main/java/com/cybertech/iface/IfaceController.java
@@ -0,0 +1,17 @@
+package com.cybertech.iface;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author wuxw
+ * @date 2018/12/13. 2:30 PM
+ */
+@RestController
+@RequestMapping("/iface")
+public class IfaceController {
+
+
+}
